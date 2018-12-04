afdcf2683c3c67359b13e9004716f7f8b9daa27f
SoItsMaestro <samuelw1994@hotmail.com>
2018-12-04 12:29:28 +0000

    Plz work

---
+++ Kostas AE3/Assets/MoveJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MoveJump : MonoBehaviour
+{
+    public float Speed;
+    public float JumpStrength;
+    public float RayLength;
+
+    public bool jumps = true;
+
+    private Rigidbody2D rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
+    {
+        rigid.velocity = new Vector2(CrossPlatformInputManager.GetAxisRaw("Horizontal") * Speed, rigid.velocity.y);
+
+        if(CrossPlatformInputManager.GetButton("Jump"))        
+        {
+            Debug.Log("Jumping");
+            if (Physics2D.Linecast(transform.position, transform.position + new Vector3(0, RayLength, 0), 1 << LayerMask.NameToLayer("Ground")))
+            {
+                Debug.Log("ForceAdded");
+                rigid.AddForce(new Vector2(0, JumpStrength));
+            }
+        }
+    }
+}
---
+++ Kostas AE3/Assets/WolfMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfMove : MonoBehaviour
+{
+    public float Speed;
+    public float Stop;
+
+    private Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, target.position) > Stop)
+        { 
+            transform.position = Vector2.MoveTowards(transform.position, target.position, Speed * Time.deltaTime);
+        }
+    }
+
+}
