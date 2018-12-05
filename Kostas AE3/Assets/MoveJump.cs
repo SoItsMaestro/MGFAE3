495374249edea5646bdfabbc0312d06de82fd258
ThatBrummie <oliverneale44@gmail.com>
2018-12-05 13:07:22 +0000

    AddedArt

--- Kostas AE3/Assets/MoveJump.cs
+++ Kostas AE3/Assets/MoveJump.cs
@@ -9,20 +9,13 @@ public class MoveJump : MonoBehaviour
     public float JumpStrength;
     public float RayLength;
 
-    public GameObject ArrowToRight, ArrowToLeft;
-    Vector2 ArrowPos;
-    public float FireRate;
-    float nextfire;
-
     public bool jumps = true;
-    public bool facingRight = true;
 
     private Rigidbody2D rigid;
 
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
-
     }
 
 
@@ -38,26 +31,6 @@ public class MoveJump : MonoBehaviour
                 Debug.Log("ForceAdded");
                 rigid.AddForce(new Vector2(0, JumpStrength));
             }
-        }
-        if (Input.GetButtonDown ("Fire1") && Time.time > nextfire)
-        {
-            nextfire = Time.time + FireRate;
-            fire ();
-        }
-    }
-
-    void fire ()
-    {
-        ArrowPos = transform.position;
-        if(facingRight)
-        {
-            ArrowPos += new Vector2(+1f, -0.43f);
-            Instantiate(ArrowToRight, ArrowPos, Quaternion.identity);
-        }
-        else
-        {
-            ArrowPos += new Vector2(-1f, -0.43f);
-            Instantiate(ArrowToLeft, ArrowPos, Quaternion.identity);
         }
     }
 }
