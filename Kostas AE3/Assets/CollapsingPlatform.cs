43461813b47fbd227770d83cff1dc758de0e4423
SoItsMaestro <33253668+SoItsMaestro@users.noreply.github.com>
2018-12-27 19:38:03 +0000

    60 changes are you for real?

---
+++ Kostas AE3/Assets/CollapsingPlatform.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollapsingPlatform : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    public float FallDuration;
+
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Player"))
+        {
+            StartCoroutine(Fall());
+        }       
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(FallDuration);
+        rigid.isKinematic = false;
+        yield return 0;
+    }
+}
---
+++ Kostas AE3/Assets/Teleporter.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour
+{
+    public GameObject Portal1;
+    public GameObject Portal2;
+    public GameObject Player;
+    public bool TeleOnce = true;   
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (TeleOnce = false)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                TeleOnce = true;
+                StartCoroutine(Teleport());
+            }
+        }
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {       
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            TeleOnce = false;
+        }
+    }
+
+    IEnumerator Teleport()
+    {
+        TeleOnce = false;
+        yield return new WaitForSeconds(1);
+        Player.transform.position = new Vector2(Portal1.transform.position.x, Portal1.transform.position.y);       
+    }
+}
