9186ea334fe3d388b78f66c28995634d50a7ed95
SoItsMaestro <33253668+SoItsMaestro@users.noreply.github.com>
2019-01-04 13:50:07 +0000

    More bits done

---
+++ Kostas AE3/Assets/Bolder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bolder : MonoBehaviour
+{
+    public GameObject Player;
+    Vector2 BolderFall;
+
+    Rigidbody2D rigid;
+    
+	
+	void Start ()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update ()
+    {
+	
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))        
+            rigid.isKinematic = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(Player);
+        }
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
