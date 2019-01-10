3af13bc9bf67ba921dd151ec51586486b975eaa0
ThatBrummie <oliverneale44@gmail.com>
2019-01-10 17:29:37 +0000

    zombie and interaction started
    zombie done, interaction is done, just need to do add a button to use the interactable.

---
+++ Kostas AE3/Assets/InteractableChest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableChest : MonoBehaviour {
+
+   public  GameObject InteractableObj = null;
+
+
+    private void Update()
+    {
+        //if (Input. ("Jump") && InteractableObj)
+        {
+            //Do somthing with the object
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag ("Interactable"))
+        {
+            Debug.Log(other.name);
+            InteractableObj = other.gameObject;
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Interactable"))
+        {
+            if(other.gameObject == InteractableObj)
+            {
+                InteractableObj = null;
+            }
+            
+        }
+    }
+}
